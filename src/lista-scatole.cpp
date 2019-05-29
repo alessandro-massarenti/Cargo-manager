9a47a299336eb4e536942fcd4afbbbccba113a07
bonny-triste <51159728+bonny-triste@users.noreply.github.com>
2019-05-29 16:20:36 +0200

    Update lista-scatole.cpp

--- src/lista-scatole.cpp
+++ src/lista-scatole.cpp
@@ -8,18 +8,53 @@
 #include <iostream>
 #include "lista-scatole.h"
 using namespace std;
-int idmax(nodo* list,int max=0){
-	    	if(!list)
-	    		return max;
-	    	if(max<list->id)
-	    		max=list->id;
-	    	return idmax(list->next,max);
+int idmax(nodo* list) {
+	if (!list)
+		return 0;
+	int max = idmax(list->next);
+	if (max < list->id)
+		max = list->id;
+	return max;
 }
-void insertList(nodo *&list,int id, int altezza, int larghezza, int profondita, int peso) {
-	if(!list)
-	list=new nodo(id, altezza, larghezza, profondita, peso);
+int altmax(nodo* list) {
+	if (!list)
+		return 0;
+	int max = altmax(list->next);
+	if (max < list->altezza)
+		max = list->altezza;
+	return max;
+}
+int larmax(nodo* list) {
+	if (!list)
+		return 0;
+	int max = larmax(list->next);
+	if (max < list->larghezza)
+		max = list->larghezza;
+	return max;
+}
+int promax(nodo* list) {
+	if (!list)
+		return 0;
+	int max = promax(list->next);
+	if (max < list->profondita)
+		max = list->profondita;
+	return max;
+}
+int pesmax(nodo* list) {
+	if (!list)
+		return 0;
+	int max = pesmax(list->next);
+	if (max < list->peso)
+		max = list->peso;
+	return max;
+}
+
+void insertList(nodo *&list, int id, int altezza, int larghezza, int profondita,
+		int peso) {
+	if (!list)
+		list = new nodo(id, altezza, larghezza, profondita, peso);
 	else
-		insertList(list->next,id,altezza, larghezza, profondita, peso);
+		insertList(list->next, id, altezza, larghezza, profondita, peso);
 }
 void displayBox(nodo *list) {
 	if (!list)
@@ -27,4 +62,3 @@ void displayBox(nodo *list) {
 	cout << list->id;
 	displayBox(list->next);
 }
-
