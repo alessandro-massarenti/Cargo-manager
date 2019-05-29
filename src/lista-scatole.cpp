8cf2857c6aa1cdbb9916beccee7e40ce3ca2d2b6
bonny-triste <51159728+bonny-triste@users.noreply.github.com>
2019-05-29 14:50:17 +0200

    Update lista-scatole.cpp

--- src/lista-scatole.cpp
+++ src/lista-scatole.cpp
@@ -8,26 +8,19 @@
 #include <iostream>
 #include "lista-scatole.h"
 using namespace std;
-
-
-void insertList(nodo *&list, int altezza, int larghezza, int profondita,int peso) {
-	if (!list) {
-		list = new nodo(0, altezza, larghezza, profondita, peso);
-		return;
-	}
-	int max_id = 0;
-	if (list->id > max_id)
-		max_id = list->id;
-	if (!list->next) {
-		list->next = new nodo(max_id + 1, altezza, larghezza, profondita, peso);
-		return;
-	}
-	insertList(list->next, altezza, larghezza, profondita, peso);
-
+int idmax(nodo* list,int max=0){
+	    	if(!list)
+	    		return max;
+	    	if(max<list->id)
+	    		max=list->id;
+	    	return idmax(list->next,max);
 }
-// Post : e' stato inserito un elemento a fine lista
-
-
+void insertList(nodo *&list,int id, int altezza, int larghezza, int profondita, int peso) {
+	if(!list)
+	list=new nodo(id, altezza, larghezza, profondita, peso);
+	else
+		insertList(list->next,id,altezza, larghezza, profondita, peso);
+}
 void displayBox(nodo *list) {
 	if (!list)
 		return;
