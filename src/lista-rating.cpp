38b1bb558777b95a2d22bc55666b31a08daa0528
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-05-31 15:51:43 +0200

    Update lista-rating.cpp

--- src/lista-rating.cpp
+++ src/lista-rating.cpp
@@ -4,4 +4,24 @@
  *  Created on: May 29, 2019
  *      Author: amassare
  */
+
+#include <iostream>
 #include "lista-rating.h"
+using namespace std;
+
+void insertRating(rating* &list, int id, int value) {
+	if (!list) {
+		list = new rating(id, value);
+		return;
+	}
+	insertRating(list->next, id, value);
+}
+
+bool changeRating(rating*& list, int id, int value) {
+	if (!list)
+		return false;
+	if (list->id == id)
+		list->value = value;
+	cout << list->id;
+	return changeRating(list->next, id, value);
+}
